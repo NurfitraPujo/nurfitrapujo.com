963e0726455ed4e5382c7bf7c0496170fe5b0b6f
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-12-15 10:50:43 +0700

    fix: try to fix next static assets and file issue

--- next.config.js
+++ next.config.js
@@ -6,13 +6,13 @@ module.exports = {
 const withImages = require("next-images");
 module.exports = withImages();
 
-module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-};
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({});
+
+module.exports = {
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+};
