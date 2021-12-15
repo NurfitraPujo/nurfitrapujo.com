9597522018f85edb39f74f2421c1c91d46961013
NurfitraPujo <fitrapujo@gmail.com>
2021-12-15 08:06:25 +0000

    Deploying to gh-pages from @ NurfitraPujo/nurfitrapujo.com@ddef50300797e4cebbfe42ea25f0780234e3b0ed 🚀

--- _next/static/YLFNfBwssXuLMCr-Ghamd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-b7481510d3fc1538.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
