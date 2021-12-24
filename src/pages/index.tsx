c6aed3ccf4765cdaa99cfd915d9e283c57d95b9f
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-12-25 06:00:17 +0700

    rewording: remove aspiring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
         <header className="relative flex flex-col align-start">
           <h2 className="font-body text-white text-xl">Hello, i am</h2>
           <h1 className="mt-0 text-5xl md:text-6xl text-white max-w-[443px] break-words">
-            Nurfitra Pujo Santiko <span className="text-blue">.</span>
+            Nurfitra Pujo Santiko<span className="text-blue">.</span>
           </h1>
           <h2 className="z-[-1] absolute text-5xl md:text-6xl text-subtle-black top-40 md:top-16 left-0 md:left-5 max-w-[443px] break-words">
             Web Developer
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
         </header>
 
         <p className="max-w-3xl text-white text-sm md:text-2xl">
-          I am web developer and aspring Software Engineer. My dream is crafting
+          I am Web Developer and Software Engineer. My dream is crafting
           product that can bring changes to the world and help millions of
           people.
         </p>
